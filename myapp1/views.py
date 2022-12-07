72b7247982c1cf1d69a924baa501a79ab45977e3
vladislavbarkovskij <barkovsky.vlad@gmail.com>
2022-12-07 21:06:25 +0300

    first commit

--- myapp1/views.py
+++ myapp1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from myapp1.models import Shop
 
 
 def zodiac_views(request):
@@ -6,63 +7,50 @@ def zodiac_views(request):
 
 
 def aries(request):
-    return HttpResponse('Это знак Овен')
+    return HttpResponse('\nЭто знак Овен')
 
 
 def leo(request):
-    return HttpResponse('Это знак Лев')
+    return HttpResponse('\nЭто знак Лев')
 
 
 def taurus(request):
-    return HttpResponse('Это знак Телец')
+    return HttpResponse('\nЭто знак Телец')
 
 
 def gemeni(request):
-    return HttpResponse('Это знак Близнецы')
+    return HttpResponse('\nЭто знак Близнецы')
 
 
 def cancer(request):
-    return HttpResponse('Это знак Рак')
+    return HttpResponse('\nЭто знак Рак')
 
 
 def virgo(request):
-    return HttpResponse('Это знак Дева')
+    return HttpResponse('\nЭто знак Дева')
 
 
 def libra(request):
-    return HttpResponse('Это знак Весы')
+    return HttpResponse('\nЭто знак Весы')
 
 
 def scorpio(request):
-    return HttpResponse('Это знак Скорпион')
+    return HttpResponse('\nЭто знак Скорпион')
 
 
 def sagittarius(request):
-    return HttpResponse('Это знак Стрелец')
+    return HttpResponse('\nЭто знак Стрелец')
 
 
 def сapricorn(request):
-    return HttpResponse('Это знак Козерог')
+    return HttpResponse('\nЭто знак Козерог')
 
 
 def aquarius(request):
-    return HttpResponse('Это знак Водолей')
+    return HttpResponse('\nЭто знак Водолей')
 
 
 def pisces(request):
-    return HttpResponse('Это знак Рыбы')
+    return HttpResponse('\nЭто знак Рыбы')
 
 
-from myapp1.models import Shop
-
-first_shop = Shop.objects.create(title="First shop")
-
-second_shop = Shop(title="First shop")
-second_shop.save()
-third_shop = Shop(title="First shop")
-third_shop.save()
-four_shop = Shop(title="First shop")
-four_shop.save()
-five_shop = Shop(title="First shop")
-five_shop.save()
-shop_list = Shop.objects.filter(id__gt=3)
